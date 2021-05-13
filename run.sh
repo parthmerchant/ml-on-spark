c9568883ed9e5a534118fa8539f47b3a7703a338
Parth Merchant <parthmmerchant@gmail.com>
2021-05-12 20:35:11 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -5,26 +5,29 @@ echo "----------------------------------------"
 
 command=$1;
 
+echo "Resetting output"
+rm ./output.txt
+
 cd datasets
 
 # DecisionTree
 if [ $1 = "DecisionTree" ]; then
-    spark-submit ../spark_jobs/decision_tree.py > ../output_directory/decision_tree_log.txt;
+    spark-submit ../spark_jobs/decision_tree.py > ../output.txt;
 fi
 
 # KMeansClustering
 if [ $1 = "KMeansClustering" ]; then
-    spark-submit ../spark_jobs/k_means_clustering.py > ../output_directory/k_means_clustering_log.txt;
+    spark-submit ../spark_jobs/k_means_clustering.py > ../output.txt;
 fi
 
 # TFIDF
 if [ $1 = "TFIDF" ]; then
-    spark-submit ../spark_jobs/tf-idf.py > ../output_directory/tf_idf_log.txt;
+    spark-submit ../spark_jobs/tf-idf.py > ../output.txt;
 fi
 
 # LinearRegression
 if [ $1 = "LinearRegression" ]; then
-    spark-submit ../spark_jobs/linear_regression.py > ../output_directory/linear_regression_log.txt;
+    spark-submit ../spark_jobs/linear_regression.py > ../output.txt;
 fi
 
 echo "Spark Job $1 COMPLETE!"
